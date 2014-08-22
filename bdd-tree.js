d66c1cff2fc695f0157ce2498ae5da91cc370b60
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-08-21 22:52:00 -0400

    initial

---
+++ bdd-tree.js
@@ -0,0 +1,5 @@
+function bddToTree(source) {
+
+}
+
+module.exports = bddToTree;
---
+++ test/bdd-tree-spec.js
@@ -0,0 +1,12 @@
+var toTree = require('../bdd-tree');
+require('lazy-ass');
+
+describe('bdd-tree', function () {
+  it('is a function', function () {
+    la(typeof toTree === 'function');
+  });
+
+  it('can process this file', function () {
+
+  });
+});
