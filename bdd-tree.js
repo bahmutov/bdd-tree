779bfe2af4029f58bc477a7fcd378f27d39dabdc
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-08-21 23:00:40 -0400

    starting source

--- bdd-tree.js
+++ bdd-tree.js
@@ -1,5 +1,8 @@
-function bddToTree(source) {
+var falafel = require('falafel');
 
+function bddToTree(source) {
+  var describes = [];
+  return describes;
 }
 
 module.exports = bddToTree;
--- test/bdd-tree-spec.js
+++ test/bdd-tree-spec.js
@@ -1,5 +1,6 @@
 var toTree = require('../bdd-tree');
 require('lazy-ass');
+var fs = require('fs');
 
 describe('bdd-tree', function () {
   it('is a function', function () {
@@ -7,6 +8,9 @@ describe('bdd-tree', function () {
   });
 
   it('can process this file', function () {
-
+    var src = fs.readFileSync(__filename, 'utf8');
+    la(typeof src === 'string');
+    var describes = toTree(src);
+    la(Array.isArray(describes));
   });
 });
