b9296f2d545929c8282037e619486403710146bb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-08-21 23:30:53 -0400

    grabbing describe blocks

--- bdd-tree.js
+++ bdd-tree.js
@@ -1,7 +1,43 @@
 var falafel = require('falafel');
+require('lazy-ass');
+
+var describes = [];
+
+function toTree(node) {
+  // console.log(node.type, node.name);
+  if (node.type === 'BlockStatement') {
+    // console.log(node.source())
+    // console.log('parent', node.parent.parent.callee.name)
+    // console.log(node.parent.parent);
+    var name = node.parent.parent.callee.name;
+    if (name === 'describe') {
+      var describeName = node.parent.parent.arguments[0].value;
+      // console.log(node.parent.parent);
+      describes.push({
+        name: describeName,
+        code: node.source()
+      });
+    }
+  }
+  /*
+  if (node.type === 'Identifier' && node.name === 'describe') {
+    // console.log(node);
+    // console.log(node.parent.arguments[1].body.body)
+    // if (node.parent.arguments.length === 2) {
+      // console.log(node.parent.arguments[1].body.source());
+      describes.push({
+        name: node.parent.arguments[0].value,
+        code: node.code
+      });
+    // }
+  }*/
+}
 
 function bddToTree(source) {
-  var describes = [];
+  describes = [];
+
+  falafel(source, toTree);
+
   return describes;
 }
 
--- test/bdd-tree-spec.js
+++ test/bdd-tree-spec.js
@@ -1,16 +1,24 @@
 var toTree = require('../bdd-tree');
 require('lazy-ass');
-var fs = require('fs');
 
 describe('bdd-tree', function () {
   it('is a function', function () {
     la(typeof toTree === 'function');
   });
 
+  var fs = require('fs');
+  var src = fs.readFileSync(__filename, 'utf8');
+
   it('can process this file', function () {
-    var src = fs.readFileSync(__filename, 'utf8');
     la(typeof src === 'string');
     var describes = toTree(src);
     la(Array.isArray(describes));
+  });
+
+  it('finds correct number describe blocks', function () {
+    var describes = toTree(src);
+    // console.log(describes)
+    la(describes.length === 1); // in this file
+    la(describes[0].name === 'bdd-tree');
   });
 });
