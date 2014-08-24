8897fa1c788a0e74d292d32c87aff7de0d0f606f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-08-23 22:43:56 -0400

    detecting describe blocks

--- bdd-tree.js
+++ bdd-tree.js
@@ -20,6 +20,12 @@ function isDescribeBlockStatement(node) {
     node.parent.parent.callee.name === 'describe';
 }
 
+function stripFirstAndLastBraces(src) {
+  return src.replace(/^\s*\{\n/, '')
+    .replace(/\}$/, '')
+    .trim();
+}
+
 function toTree(node) {
   // console.log(node.type, node.name);
   if (node.type === 'BlockStatement') {
@@ -29,9 +35,11 @@ function toTree(node) {
     if (isDescribeBlockStatement(node)) {
       var describeName = node.parent.parent.arguments[0].value;
       // console.log(node.parent.parent);
+      var src = node.source();
+      src = stripFirstAndLastBraces(src);
       describes.push({
         name: describeName,
-        code: node.source()
+        code: src
       });
     }
   }
